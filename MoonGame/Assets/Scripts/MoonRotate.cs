86be26d23471208321d107f44a69ed28f9b8ec75
Jessy Ruiter <jessyruiter@gmail.com>
2019-03-03 21:29:02 +1300

    Moon Animation
    Added a placeholder sprite for the moon, and implemented code for it to rotate upon button presses.

---
+++ MoonGame/Assets/Scripts/MoonRotate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonRotate : MonoBehaviour {
+
+	public KeyCode pressLeft;
+
+	public KeyCode pressRight;
+
+	public Sprite moonSprite;
+
+	//public float ZAngle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("left"))
+        {
+            transform.Rotate(0, 0, 5 * -10 * Time.deltaTime);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, 0, 5 * 10 * Time.deltaTime);
+        }
+    }
+}
