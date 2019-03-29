43c1f062508aad7f9017d3018c1d267c2ad7a28e
Jessy Ruiter <jessyruiter@gmail.com>
2019-03-29 21:05:59 +1300

    Basic Inventory
    Attached a simple inventory to the player that creates an empty array and has functionality to add items to the array.

---
+++ MoonGame/Assets/Scripts/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    public GameObject[] inventory = new GameObject[4];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void addItem(GameObject item)
+    {
+        bool isAdd = false;
+        //locate the first free slot
+        for(int i = 0; i < inventory.Length; i++)
+        {
+            if(inventory[i] == null)
+            {
+                inventory[i] = item;
+                isAdd = true;
+                break;
+            }
+            
+        }
+        if (!isAdd)
+        {
+            //inventory full
+            Debug.Log("Inventory is already full");
+        }
+    }
+}
